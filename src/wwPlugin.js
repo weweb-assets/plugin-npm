fcd60552b796b5dccbbf9951fb7f1e7b41200eca
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2023-08-04 14:52:04 +0200

    Fix functions

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -8,42 +8,35 @@ export default {
     packages: {},
 
     onLoad() {
-        addScripts(this.settings.publicData.packages, wwLib.getFrontDocument());
+        this.addScripts(this.settings.publicData.packages, wwLib.getFrontDocument());
     },
 
-    loadPackage({ data, fileName }, wwUtils) {
-        addScript(this.settings.publicData.packages[0], wwLib.getFrontDocument());
-
-        data = { test: 'test' };
-        /* wwEditor:start */
-        wwUtils && wwUtils.log({ label: 'Data', preview: data });
-        /* wwEditor:end */
-
-        console.log({ data, fileName }, wwUtils);
+    loadPackage() {
+        this.addScript(this.settings.publicData.packages[0], wwLib.getFrontDocument());
     },
 
     load() {
-        addScripts(this.settings.publicData.packages, wwLib.getFrontDocument());
+        this.addScripts(this.settings.publicData.packages, wwLib.getFrontDocument());
+    },
+
+    addScript(packageItem, context) {
+        const packageSrc = `https://unpkg.com/${packageItem.name}@${packageItem.version}`;
+        const existingScript = context.querySelector(`script[src="${packageSrc}"]`);
+        if (existingScript) return;
+
+        const script = context.createElement('script');
+        script.type = 'text/javascript';
+        script.src = packageSrc;
+        script.onload = () => {
+            this.packages[packageItem.name] = wwLib.getFrontWindow()[packageItem.name];
+        };
+
+        context.head.appendChild(script);
+    },
+
+    addScripts(packages, context) {
+        for (const packageItem of packages) {
+            if (packageItem.auto) addScript(packageItem, context);
+        }
     },
 };
-
-function addScript(packageItem, context) {
-    const packageSrc = `https://unpkg.com/${packageItem.name}@${packageItem.version}`;
-    const existingScript = context.querySelector(`script[src="${packageSrc}"]`);
-    if (existingScript) return;
-
-    const script = context.createElement('script');
-    script.type = 'text/javascript';
-    script.src = packageSrc;
-    script.onload = () => {
-        packages[packageItem.name] = wwLib.getFrontWindow()[packageItem.name];
-    };
-
-    context.head.appendChild(script);
-}
-
-function addScripts(packages, context) {
-    for (const packageItem of packages) {
-        if (packageItem.auto) addScript(packageItem, context);
-    }
-}
