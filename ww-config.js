b4f073aa883718a45e4ae301465f3eb08befb63e
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:23:37 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -11,6 +11,36 @@ export default {
                 );
             },
             onSave: 'reloadPackages',
+            /* wwEditor:start */
+            copilot: {
+                description: 'Loads and manages NPM packages in your project',
+                returns: 'object',
+                schema: {
+                    packages: {
+                        type: 'array',
+                        description: 'Array of NPM packages to load',
+                        bindable: false,
+                        schema: {
+                            name: {
+                                type: 'string',
+                                description: 'Name of the NPM package',
+                                bindable: false
+                            },
+                            version: {
+                                type: 'string',
+                                description: 'Version of the NPM package',
+                                bindable: false
+                            },
+                            instanceName: {
+                                type: 'string',
+                                description: 'Global variable name to access the package',
+                                bindable: false
+                            }
+                        }
+                    }
+                }
+            }
+            /* wwEditor:end */
         },
     },
     variables: settings => {
